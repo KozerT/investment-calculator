fd80152e2752eaf9e2d19189835cbdfa01c30108
KozerT <kozer.vizastaff@gmail.com>
2024-01-09 11:22:50 +0100

    feat: Added UserInput component and styled it

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,27 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserInput = () => {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const handleUserInput = (inputId, newValue) => {
+    setUserInput((prev) => {
+      return {
+        ...prev,
+        [inputId]: newValue,
+      };
+    });
+  };
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label htmlFor="">Initial Investment</label>
-          <input type="text" />
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) => {
+              handleUserInput("initialInvestment", event.target.value);
+            }}
+          />
         </p>
         <p>
           <label htmlFor="">Annual Investment</label>
-          <input type="text" />
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) => {
+              handleUserInput("annualInvestment", event.target.value);
+            }}
+          />
         </p>
       </div>
 
       <div className="input-group">
         <p>
-          <label htmlFor="">Initial Investment</label>
-          <input type="text" />
+          <label htmlFor="">Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) => {
+              handleUserInput("expectedReturn", event.target.value);
+            }}
+          />
         </p>
         <p>
-          <label htmlFor="">Annual Investment</label>
-          <input type="text" />
+          <label htmlFor="">Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => {
+              handleUserInput("duration", event.target.value);
+            }}
+          />
         </p>
       </div>
     </section>
